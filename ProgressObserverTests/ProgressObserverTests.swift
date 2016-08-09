a8ebba70463c583e16619ea53b158838fb08af43
Ayush Newatia <ayush.newatia@icloud.com>
2016-08-09 23:22:53 +0100

    added unit tests

--- ProgressObserverTests/ProgressObserverTests.swift
+++ ProgressObserverTests/ProgressObserverTests.swift
@@ -11,26 +11,102 @@ import XCTest
 
 class ProgressObserverTests: XCTestCase {
     
+    var progressObserver: ProgressObserver!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        progressObserver = ProgressObserver(totalUnitCount: 100)
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testParentChildIniitaliser() {
+        let childObserver = ProgressObserver(totalUnitCount: 100, parent: progressObserver, pendingUnitCount: 50)
+        
+        childObserver.completedUnitCount = 50
+        
+        XCTAssert(progressObserver.fractionCompleted == 0.25)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    func testTotalUnitCountInitialiser() {
+        progressObserver.completedUnitCount = 25
+        XCTAssert(progressObserver.fractionCompleted == 0.25)
+    }
+    
+    func testSettingDelegate() {
+        class MockDelegate: ProgressObserverDelegate {}
+        progressObserver.delegate = MockDelegate()
+        
+        XCTAssert(progressObserver.delegate != nil)
+        XCTAssert(progressObserver.isObserving == true)
+    }
+    
+    func testUnsettingDelegate() {
+        class MockDelegate: ProgressObserverDelegate {}
+        progressObserver.delegate = MockDelegate()
+        progressObserver.delegate = nil
+        
+        XCTAssert(progressObserver.delegate == nil)
+        XCTAssert(progressObserver.isObserving == false)
+    }
+    
+    func testSuccessfulDeinit() {
+        class MockDelegate: ProgressObserverDelegate {}
+        progressObserver.delegate = MockDelegate()
+        progressObserver = nil
+        
+        XCTAssert(progressObserver == nil)
+    }
+    
+    func testFractionCompletedNotification() {
+        let expectation = expectationWithDescription("The fractionCompleted notification should be triggered")
+        
+        class MockDelegate: ProgressObserverDelegate {
+            let expectation: XCTestExpectation
+            
+            init(expectation: XCTestExpectation) {
+                self.expectation = expectation
+            }
+            
+            func progressObserver(observer: ProgressObserver,
+                                          didUpdateFractionCompleted fraction: Double,
+                                          withUserInfo userInfo: [NSObject : AnyObject]) {
+                if fraction == 0.25 { expectation.fulfill() }
+            }
+        }
+        
+        progressObserver.delegate = MockDelegate(expectation: expectation)
+        progressObserver.completedUnitCount = 25
+        
+        waitForExpectationsWithTimeout(5) { error in
+            XCTAssertNil(error, "The fractionCompleted notification was not triggered")
         }
     }
     
+    func testCompletedUnitNotification() {
+        let expectation = expectationWithDescription("The completedUnitCount notification should be triggered")
+        
+        class MockDelegate: ProgressObserverDelegate {
+            let expectation: XCTestExpectation
+            
+            init(expectation: XCTestExpectation) {
+                self.expectation = expectation
+            }
+            
+            func progressObserver(observer: ProgressObserver,
+                                  didUpdateCompletedUnitCount unitCount: Int64,
+                                  withUserInfo userInfo: [NSObject : AnyObject]) {
+                if unitCount == 25 { expectation.fulfill() }
+            }
+        }
+        
+        progressObserver.delegate = MockDelegate(expectation: expectation)
+        progressObserver.completedUnitCount = 25
+        
+        waitForExpectationsWithTimeout(5) { error in
+            XCTAssertNil(error, "The completedUnitCount notification was not triggered")
+        }
+    }
 }
